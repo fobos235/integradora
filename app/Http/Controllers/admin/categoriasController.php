82755263092cc2b965e171027f47d685c972bd95
Arturo Andrade <arturoandradm@gmail.com>
2020-08-19 11:49:07 -0500

    Recuperado categoriasController

--- app/Http/Controllers/admin/categoriasController.php
+++ app/Http/Controllers/admin/categoriasController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoPost;
 use App\Productos;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Redirect; 
 
 class categoriasController extends Controller
 {
